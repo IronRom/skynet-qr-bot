b81e9d395f40c69ecb0acaea8660766cf64f7064
Roman Yarkey <r.yarkey@gmail.com>
2025-08-19 00:08:48 +0700

    First test for build and deploy workflow

--- bot/bot.py
+++ bot/bot.py
@@ -26,6 +26,7 @@ from aiogram.fsm.storage.redis import RedisStorage
 from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
 from redis.asyncio import Redis
 
+# comment for commit
 async def main(
         config: Config,
         session_factory: async_sessionmaker
