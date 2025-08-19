1c86145e95bc9de85359aa15e771025d576078dd
Roman Yarkey <r.yarkey@gmail.com>
2025-08-19 12:07:27 +0700

    TERRAFORM - add ecs execution secrets IAM policy

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -172,6 +172,32 @@ resource "aws_iam_role" "ecs_task_execution" {
   })
 }
 
+resource "aws_iam_policy" "ecs_execution_secrets" {
+  name = "EcsExecutionSecretsPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = [
+          aws_secretsmanager_secret.bot_token.arn,
+          aws_secretsmanager_secret.db_dsn.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_secrets_attach" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ecs_execution_secrets.arn
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
