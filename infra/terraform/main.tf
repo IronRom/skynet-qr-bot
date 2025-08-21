a5511cfc7d2c74e51e3d74dc9c3d59a3ef36333b
Roman Yarkey <r.yarkey@gmail.com>
2025-08-21 13:00:45 +0700

    TERRAFORM - Test RDS Creation 7

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -192,7 +192,7 @@ resource "aws_db_instance" "skynet" {
   engine               = "postgres"
   engine_version       = "15.3"
   instance_class       = "db.t4g.micro"
-  name                 = var.db_name
+  identifier           = var.db_name
   username             = var.db_user
   password             = var.db_password
   skip_final_snapshot  = true
