9f355787818b2135f2dc42b54d64c6f95bc74da5
Roman Yarkey <r.yarkey@gmail.com>
2025-08-22 12:55:36 +0700

    TERRAFORM - VPC Endpoints for ECR

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -105,6 +105,25 @@ resource "aws_internet_gateway" "igw" {
 }
 
 ##########################
+# VPC Endpoints для ECR
+##########################
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.eu-central-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.private.id, aws_subnet.private_b.id]
+  security_group_ids = [aws_security_group.ecs_tasks_private.id]
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.eu-central-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [aws_subnet.private.id, aws_subnet.private_b.id]
+  security_group_ids = [aws_security_group.ecs_tasks_private.id]
+}
+
+##########################
 # Route Tables
 ##########################
 resource "aws_route_table" "public" {
